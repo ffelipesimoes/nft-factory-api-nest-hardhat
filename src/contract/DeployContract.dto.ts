d87c8583bd0f90214df386b68364879ff9d9a0e5
Felipe Simões <ffelipesimoes@gmail.com>
2023-12-20 18:18:02 +0000

    docs: adding documentation

--- src/contract/DeployContract.dto.ts
+++ src/contract/DeployContract.dto.ts
@@ -1,13 +1,29 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsNotEmpty, IsString } from 'class-validator';
 export class DeployContractDto {
+  @ApiProperty({
+    description: 'Network Name',
+    example: 'sepolia',
+    default: 'sepolia',
+  })
   @IsNotEmpty()
   @IsString()
   networkName: string;
 
+  @ApiProperty({
+    description: 'Token Name',
+    example: 'Token do Leao',
+    default: 'Meu token',
+  })
   @IsNotEmpty()
   @IsString()
   tokenName: string;
 
+  @ApiProperty({
+    description: 'Token Symbol',
+    example: 'NFT',
+    default: 'NFTT',
+  })
   @IsNotEmpty()
   @IsString()
   tokenSymbol: string;
--- src/contract/contract.controller.ts
+++ src/contract/contract.controller.ts
@@ -2,11 +2,24 @@ import { Controller, Post, Body } from '@nestjs/common';
 import { ContractService, DeployedContractProps } from './contract.service';
 import { CONTRACT_ADRRESS_EXPLORER_URLS } from '../networks';
 import { DeployContractDto } from './DeployContract.dto';
+import {
+  ApiBadRequestResponse,
+  ApiCreatedResponse,
+  ApiTags,
+} from '@nestjs/swagger';
 
 export interface DeployedContractControllerProps {
   response: DeployedContractProps;
   url: string;
 }
+
+@ApiTags('Deploy Contract')
+@ApiCreatedResponse({
+  status: 201,
+  description: 'Contract deployed',
+  type: DeployContractDto,
+})
+@ApiBadRequestResponse({ status: 500, description: 'Deu ruim' })
 @Controller('contract')
 export class ContractController {
   constructor(private readonly contractService: ContractService) {}
